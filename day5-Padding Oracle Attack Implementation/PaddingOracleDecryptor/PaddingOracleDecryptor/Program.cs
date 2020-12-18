dfb81fb7b23b39267b7fcef2cc3f1b7ee2a5da8a
JackBai <xingjianbai0914@sina.com>
2020-12-18 16:00:03 +0000

    day3-day4

---
+++ day5-Padding Oracle Attack Implementation/PaddingOracleDecryptor/PaddingOracleDecryptor/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * author: martani (martani.net@gmail.com)
+ * copyright 2013
+ * 
+ */
+using System;
+//using PaddingOracleAttackLib;
+using Attack;
+
+
+namespace PaddingOracle
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Attack.Oracles.AES_CBCOracle aes = new Attack.Oracles.AES_CBCOracle ();
+            Attack.Attacker attacker = new Attack.Attacker (aes);
+            string clearText = "And what, Socrates, is the food of the soul? Surely, I said, knowledge is the food of the soul. -- Plato";
+            byte[] cipher = aes.AES_EncryptString (clearText);
+
+            string plainText = attacker.Decrypt(cipher);
+            Console.WriteLine("\n>>>>>>>>> Decryption result <<<<<<<<<<<:\n{0}", plainText);
+
+            Console.ReadKey();
+        }
+    }
+}
